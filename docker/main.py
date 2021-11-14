714907b2195c8bca1aae28d18e050a049bdff2fa
admiswalker <admiswalker@gmail.com>
2021-11-14 22:47:02 +0900

    add test

--- docker/main.py
+++ docker/main.py
@@ -1,6 +1,14 @@
 import cv2 as cv
 
+def test():
+    img = cv.imread('./test_in.png')
+    print(type(img))
+    print(img.shape)
+    img_rotate_180 = cv.rotate(img, cv.ROTATE_180)
+    cv.imwrite('test_out.png', img_rotate_180)
+
 def main():
     print(cv.__version__)
+    test()
 
 main()
